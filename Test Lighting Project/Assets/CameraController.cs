a71602c14883d75c1dc201bb33acc4289d60415b
bslease <bslease@gmail.com>
2022-02-12 15:10:53 -0700

    implement screensaver mode

--- Test Lighting Project/Assets/CameraController.cs
+++ Test Lighting Project/Assets/CameraController.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float RotateSpeed = 1.0f;
+    public float RotateSpeed = 20.0f;
+    public float IdleRotateSpeed = 5.0f;
+    public float maxIdleTime = 10.0f;
+    private float timeSinceInput = 0.0f;
 
     void Update()
     {
         float hInput = Input.GetAxis("Horizontal");
-        transform.Rotate(new Vector3(0, -hInput * RotateSpeed * Time.deltaTime, 0));
+        if (hInput == 0.0f)
+        {
+            timeSinceInput += Time.deltaTime;
+            if (timeSinceInput > maxIdleTime)
+            {
+                transform.Rotate(new Vector3(0, IdleRotateSpeed * Time.deltaTime, 0));
+            }
+        }
+        else
+        {
+            timeSinceInput = 0.0f;
+            transform.Rotate(new Vector3(0, -hInput * RotateSpeed * Time.deltaTime, 0));
+        }
     }
 }
