0d0456a7568ffddaeb83546e75e30fd430f32be0
bslease <bslease@gmail.com>
2022-02-13 11:08:44 -0700

    add light probes demo scene

---
+++ Test Lighting Project/Assets/BoxController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public float moveSpeed = 20.0f;
+    public float jumpForce = 5.0f;
+    private Rigidbody rb;
+    private bool doJump;
+
+    private void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            doJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        rb.MovePosition(transform.position + new Vector3(hInput * moveSpeed * Time.fixedDeltaTime, 0, vInput * moveSpeed * Time.fixedDeltaTime));
+        if (doJump)
+        {
+            doJump = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
